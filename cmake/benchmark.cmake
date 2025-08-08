445b6aeaf3cc53962ffb984035b47f96e27b6df1
Nicolás Serrano García <serranogarcianicolas@gmail.com>
2025-08-08 14:29:19 +0200

    Added support for benchmarks

---
+++ cmake/benchmark.cmake
@@ -0,0 +1,14 @@
+option(BUILD_BENCHMARK "build benchmark" ON)
+
+if(BUILD_BENCHMARK)
+    message(STATUS "Building benchmark is ENABLED")
+    #add support for tests. Inside the CMakeLists.txt
+    #inside test/, there are all the test modules.
+    #using this structure, different flags can be defined
+    #to only import certain tests to be run.
+    add_subdirectory(${CMAKE_SOURCE_DIR}/benchmark)
+
+else()
+    message(STATUS "Building benchmark is DISABLED")
+endif()
+
