4fdc731fc83f58cbd355152fbd48b066176ad005
Nicolás Serrano García <serranogarcianicolas@gmail.com>
2025-08-09 22:34:08 +0200

    refactor (CMakeLists.txt): disabled downloading CTest or benchmarks if not necessary
    Even if the flags were disabled, CMake would still include the modules

--- cmake/benchmark.cmake
+++ cmake/benchmark.cmake
@@ -1,4 +1,3 @@
-option(BUILD_BENCHMARK "build benchmark" OFF)
 
 if(BUILD_BENCHMARK)
     message(STATUS "Building benchmark is ENABLED")
@@ -6,6 +5,19 @@ if(BUILD_BENCHMARK)
     #inside test/, there are all the test modules.
     #using this structure, different flags can be defined
     #to only import certain tests to be run.
+    
+
+    #---Add google benchmark with CPM---#
+
+    include(CPM.cmake)
+
+    CPMAddPackage(
+        NAME benchmark
+        GITHUB_REPOSITORY google/benchmark
+        GIT_TAG v1.7.1
+        OPTIONS "BENCHMARK_ENABLE_TESTING OFF"
+    )
+
     add_subdirectory(${CMAKE_SOURCE_DIR}/benchmark)
 
 else()
